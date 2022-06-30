ca1c1e9ece5567fe8582ed5f850fbd43995b5054
KevinDM14 <kevindiazm.14@gmail.com>
2022-06-30 10:29:09 -0500

    mostrar nombre del juego

--- src/codebreaker/codebreaker.test.js
+++ src/codebreaker/codebreaker.test.js
@@ -1,8 +1,12 @@
 import codebreaker from "./codebreaker"
+import fs from 'fs'
 
 describe("Code breaker", () => {
   it("Que muestre el titulo", () => {
-    
+    document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+    const h1 = document.querySelector("#titulo");
+
+    expect(h1.innerHTML).toEqual("Codebreaker");
   })
 })
 
