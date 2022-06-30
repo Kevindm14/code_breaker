10b899c8e699337d753c42a09a9ba45111f4a9a1
KevinDM14 <kevindiazm.14@gmail.com>
2022-06-30 10:34:30 -0500

    cambiar title del documento

--- src/codebreaker/codebreaker.test.js
+++ src/codebreaker/codebreaker.test.js
@@ -8,5 +8,12 @@ describe("Code breaker", () => {
 
     expect(h1.innerHTML).toEqual("Codebreaker");
   })
+
+  it("Cambiar title del documento", () => {
+    document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+    const title = document.querySelector("title");
+
+    expect(title.innerHTML).toEqual("Codebreaker");
+  })
 })
 
